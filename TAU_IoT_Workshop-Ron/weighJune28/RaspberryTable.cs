0619ba10c275c38797ee221940ccc03650f2f88e
Ramy Lerner <san85@walla.com>
2017-08-20 13:53:12 +0300

    hi

---
+++ TAU_IoT_Workshop-Ron/weighJune28/RaspberryTable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace weighJune28
+{
+    class RaspberryTable
+    {
+        public string Id { get; set; }
+
+        public DateTime createdAt { get; set; }
+
+        [JsonProperty(PropertyName = "IPNumber")]
+        public long IPNumber { get; set; }
+
+
+        [JsonProperty(PropertyName = "MacAddress")]
+        public long MacAddress { get; set; }
+
+    }
+}
